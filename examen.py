09cae22bd527be0b809215cab073b31266a6f6f5
rolinss1 <rolinss@yahoo.com>
2026-08-02 00:30:25 -0500

    Update examen.py

--- examen.py
+++ examen.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Configuración visual de la ventana
 st.set_page_config(page_title="Simulador MINEDU - Ascenso 2024", page_icon="imagen rolinss3.jpg", layout="centered")
-st.sidebar.image("imagen rolinss3.jpg", width=120)
+st.sidebar.image("imagen rolinss3.jpg", width=120 caption="Profesor(a) Administrador(a)")
 st.title("Simulador de Examen MINEDU")
 st.subheader("Concurso de Ascenso 2024 - Educación Religiosa Secundaria")
 st.write("Responde las preguntas y obtén tu calificación instantánea al finalizar.")
